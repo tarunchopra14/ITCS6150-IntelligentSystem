81999c0da8ee9dc39a9f8cbe235e8ce7b24a58b7
Jared Tamulynas <jaredt93@vt.edu>
2023-09-17 20:59:56 -0400

    ai
    ai improvements

--- game_ai.py
+++ game_ai.py
@@ -10,6 +10,9 @@ Functions:
 
 import game_logic
 
+
+GOOD_ENOUGH_THRESHOLD = 300  # Adjust as per requirements
+
 def alpha_beta(player, depth, alpha, beta, board):
     """
     Implement the alpha-beta pruning algorithm to find the best move.
@@ -24,15 +27,16 @@ def alpha_beta(player, depth, alpha, beta, board):
     Returns:
         tuple: Best value calculated by the algorithm and the best move as (z, x, y).
     """
-    # Base cases: End search if depth is reached or if a win is detected for either player.
-    if depth == 0 or game_logic.check_win(1, board) or game_logic.check_win(-1, board):
-        return game_logic.evaluate(board), None
+        
+    if depth == 0 or game_logic.check_win(1, board) or game_logic.check_win(-1, board):  # Terminal state or depth limit reached
+        return game_logic.evaluate(board, depth), None
 
     best_move = None
-    
+
     # Logic for MAX player (i.e., our player)
     if player == 1:
         best_value = float('-inf')
+        
         # Check all possible moves
         for z in range(4):
             for x in range(4):
@@ -41,17 +45,25 @@ def alpha_beta(player, depth, alpha, beta, board):
                         board[z][x][y] = player  # make a move
                         value, _ = alpha_beta(-1, depth - 1, alpha, beta, board)  # simulate opponent's turn
                         board[z][x][y] = 0  # undo the move
+
                         # Update best value and move if needed
                         if value > best_value:
                             best_value = value
                             best_move = (z, x, y)
+
+                        # If a move is found that's "good enough", break out
+                        if best_value >= GOOD_ENOUGH_THRESHOLD / depth:
+                            return best_value, best_move
+
                         # Update alpha and prune if necessary
                         alpha = max(alpha, best_value)
                         if beta <= alpha:
                             break  # pruning
+
     # Logic for MIN player (i.e., the opponent)
     else:
         best_value = float('inf')
+        
         # Check all possible moves
         for z in range(4):
             for x in range(4):
@@ -60,13 +72,19 @@ def alpha_beta(player, depth, alpha, beta, board):
                         board[z][x][y] = player  # make a move
                         value, _ = alpha_beta(1, depth - 1, alpha, beta, board)  # simulate our turn
                         board[z][x][y] = 0  # undo the move
+
                         # Update best value and move if needed
                         if value < best_value:
                             best_value = value
                             best_move = (z, x, y)
+
+                        # If a move is found that's "bad enough", break out
+                        if best_value <= -GOOD_ENOUGH_THRESHOLD / depth:
+                            return best_value, best_move
+
                         # Update beta and prune if necessary
                         beta = min(beta, best_value)
                         if beta <= alpha:
                             break  # pruning
-
+    
     return best_value, best_move
