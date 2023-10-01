dbc3f5ca92057de06a7330f12aaae8d3231c5209
Jared Tamulynas <jaredt93@vt.edu>
2023-10-01 18:46:39 -0400

    add comments

--- game_ui.py
+++ game_ui.py
@@ -1,9 +1,37 @@
+"""
+game_ui.py
+
+This file manages the User Interface (UI) for the 3D Tic Tac Toe game. It handles the rendering of the board, user inputs,
+and integrates with the game logic and AI logic for gameplay.
+
+Key Features:
+    - Renders the 3D board with animated movements.
+    - Interprets user inputs to make moves.
+    - Displays game status updates (e.g., win, lose, draw).
+    - Provides different difficulty levels for the game AI.
+
+Functions:
+    - initialize_board: Prepares an empty game board.
+    - draw_board: Renders the current state of the board on the screen.
+    - get_clicked_cell: Determines which cell was clicked by the player.
+    - update_board: Updates the board's state based on a move.
+    - draw_circle_in_cell: Renders a player's move on the board.
+    - draw_title: Displays the game title.
+    - draw_play_button: Renders the Play button.
+    - draw_buttons: Renders difficulty selection buttons.
+    - get_clicked_difficulty: Determines the difficulty selected by the player.
+    - draw_reset_button: Renders the Reset game button.
+    - reset_game: Resets the game to its initial state.
+"""
+
 import pygame
 import numpy as np
 
 pygame.init()
 
-# BOARD CONSTANTS
+# --- CONSTANTS DEFINITION ---
+
+# Constants for board dimensions, styles, and colors
 LEVELS = 4
 WIDTH, HEIGHT = 750, 750
 GRID_SIZE = 4
@@ -17,18 +45,34 @@ LINE_COLOR = (0, 255, 0)
 PLAYER_X_COLOR = (255, 0, 0)
 PLAYER_O_COLOR = (0, 0, 255)
 
+# Layout configurations for the 3D board's top-left, top-right, etc.
 top_lefts = [(WIDTH // 1.33 - TOP_WIDTH // 2, PADDING + level * (4 * BASE_CELL_HEIGHT + PADDING)) for level in range(LEVELS)]
 top_rights = [(WIDTH // 1.33 + TOP_WIDTH // 2, PADDING + level * (4 * BASE_CELL_HEIGHT + PADDING)) for level in range(LEVELS)]
 bottom_lefts = [(WIDTH // 1.33 - BOTTOM_WIDTH // 2, PADDING + level * (4 * BASE_CELL_HEIGHT + PADDING) + 4 * BASE_CELL_HEIGHT) for level in range(LEVELS)]
 bottom_rights = [(WIDTH // 1.33 + BOTTOM_WIDTH // 2, PADDING + level * (4 * BASE_CELL_HEIGHT + PADDING) + 4 * BASE_CELL_HEIGHT) for level in range(LEVELS)]
 
+# Initialize Pygame display with specified dimensions
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("3D Tic Tac Toe")
 
+# --- FUNCTIONS DEFINITION ---
+
 def initialize_board():
+    """
+    Initializes and returns an empty 3D Tic Tac Toe board.
+    
+    Returns:
+        list: A 3D list representing the game board where 0 denotes an empty cell.
+    """
     return [[[0 for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)] for _ in range(LEVELS)]
 
 def draw_board(board):
+    """
+    Renders the 3D Tic Tac Toe board based on its current state.
+
+    Args:
+        board (list): The 3D list representing the game board.
+    """
     for level in range(LEVELS):
         top_left = top_lefts[level]
         top_right = top_rights[level]
@@ -76,6 +120,16 @@ def draw_board(board):
                     draw_circle_in_cell(level, row, col, player)
 
 def get_clicked_cell(pos):
+    """
+    Determines which cell of the 3D Tic Tac Toe board was clicked by the player.
+
+    Args:
+        pos (tuple): The x and y screen coordinates of the click event.
+
+    Returns:
+        tuple: A 3-tuple (level, row, col) representing the clicked cell's position.
+               Returns None if the click is outside any valid cell.
+    """
     x, y = pos
 
     # Determine level
@@ -105,16 +159,39 @@ def get_clicked_cell(pos):
     return level, int(row), int(col)
 
 
+# Constants to represent the different players.
+# PLAYER with value 1 represents the human player, while AI with value -1 represents the computer player.
 PLAYER = 1
 AI = -1
 
 def update_board(level, row, col, player):
+    """
+    Update the board's state based on a move.
+
+    Args:
+        level (int): The selected level of the board (0 to 3).
+        row (int): The row number where the move was made.
+        col (int): The column number where the move was made.
+        player (int): The player making the move (either PLAYER or AI).
+
+    Returns:
+        bool: True if the move was successful (cell was empty), False otherwise.
+    """
     if board[level][row][col] == 0:  # Check if the cell is empty
         board[level][row][col] = player
         return True  # Successfully updated
     return False  # Cell is already occupied
 
 def draw_circle_in_cell(level, row, col, player):
+    """
+    Render a player's move (circle) on the specified cell of the board.
+
+    Args:
+        level (int): The selected level of the board (0 to 3).
+        row (int): The row number where the move was made.
+        col (int): The column number where the move was made.
+        player (int): The player making the move (either PLAYER or AI).
+    """
     # Determine center X using interpolation between the left and right x-coordinates of the clicked row
     left_x = top_lefts[level][0] + row / GRID_SIZE * (bottom_lefts[level][0] - top_lefts[level][0])
     right_x = top_rights[level][0] + row / GRID_SIZE * (bottom_rights[level][0] - top_rights[level][0])
@@ -130,7 +207,6 @@ def draw_circle_in_cell(level, row, col, player):
     pygame.draw.circle(screen, color, (int(center_x), int(center_y)), 10)
 
 
-
 # Title properties
 TITLE_FONT = pygame.font.SysFont("Arial", 40)
 TITLE_COLOR = (255, 255, 255)  # White color
@@ -139,7 +215,9 @@ title_surface = TITLE_FONT.render(TITLE_TEXT, True, TITLE_COLOR)
 TITLE_POSITION = (WIDTH // 4 - title_surface.get_width() // 2, PADDING)
 
 def draw_title():
-    """Draw the game title on the screen."""
+    """
+    Renders the game title on the screen.
+    """
     screen.blit(title_surface, TITLE_POSITION)
 
 # Constants for buttons
@@ -179,7 +257,9 @@ buttons = {
 }
 
 def draw_play_button():
-    """Draw the Play button on the screen."""
+    """
+    Renders the Play button on the main screen. Changes color when hovered.
+    """
     rect = play_button["rect"]
     pygame.draw.rect(screen, BUTTON_COLORS["hover"] if rect.collidepoint(pygame.mouse.get_pos()) else BUTTON_COLORS["default"], rect)
     label = FONT.render(play_button["label"], True, BUTTON_FONT_COLOR)
@@ -187,7 +267,9 @@ def draw_play_button():
     pygame.display.flip()
 
 def draw_buttons():
-    """Draw difficulty buttons on the screen."""
+    """
+    Renders the difficulty selection buttons on the screen. Changes color on hover.
+    """
     for button_name, button in buttons.items():
         pygame.draw.rect(screen, BUTTON_COLORS["hover"] if button["rect"].collidepoint(pygame.mouse.get_pos()) else BUTTON_COLORS["default"], button["rect"])
         label = FONT.render(button["label"], True, BUTTON_FONT_COLOR)
@@ -195,7 +277,15 @@ def draw_buttons():
     pygame.display.flip()
 
 def get_clicked_difficulty(pos):
-    """Determine which difficulty button was clicked."""
+    """
+    Determines and returns the difficulty level selected by the player.
+
+    Args:
+        pos (tuple): The x and y coordinates of the click event.
+
+    Returns:
+        int: The difficulty level selected, or None if no valid difficulty button was clicked.
+    """
     for button_name, button in buttons.items():
         if button["rect"].collidepoint(pos):
             return button["difficulty"]
