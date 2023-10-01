349fa98a5f9cc1d1468a971c949603ae1194efd4
Mrinalini Chava <mrinalinichava@Mrinalinis-MBP.lan>
2023-10-01 12:04:31 -0400

    intitial test

--- game_ai.py
+++ game_ai.py
@@ -12,79 +12,129 @@ import game_logic
 
 GOOD_ENOUGH_THRESHOLD = 500  # Adjust as per requirements
 
-def alpha_beta(player, depth, alpha, beta, board):
-    """
-    Implement the alpha-beta pruning algorithm to find the best move.
+def alpha_beta(player, depth, alpha, beta,board):
+    if depth == 0 or game_logic.check_win(1,board) or game_logic.check_win(-1,board):
+        return game_logic.evaluate(board), None
 
-    Args:
-        player (int): 1 for the MAX player, -1 for the MIN player.
-        depth (int): Depth to search for the alpha-beta pruning.
-        alpha (float): Alpha value for the algorithm.
-        beta (float): Beta value for the algorithm.
-        board (list): The current state of the game board.
-
-    Returns:
-        tuple: Best value calculated by the algorithm and the best move as (z, x, y).
-    """
-    if depth == 0 or game_logic.check_win(1, board) or game_logic.check_win(-1, board):  # Terminal state or depth limit reached
-        return game_logic.evaluate(board, depth), None
-
-    best_move = None
-
-    # Logic for MAX player (i.e., our player)
-    if player == 1:
-        best_value = float('-inf')
-        
-        # Check all possible moves
-        for z in range(4):
-            for x in range(4):
-                for y in range(4):
-                    if board[z][x][y] == 0:  # if cell is empty
-                        board[z][x][y] = player  # make a move
-                        value, _ = alpha_beta(-1, depth - 1, alpha, beta, board)  # simulate opponent's turn
-                        board[z][x][y] = 0  # undo the move
-
-                        # Update best value and move if needed
-                        if value > best_value:
-                            best_value = value
-                            best_move = (z, x, y)
-
-                        # If a move is found that's "good enough", break out
-                        print("CHECK MAX " + str(best_value >= GOOD_ENOUGH_THRESHOLD))
-                        if best_value >= GOOD_ENOUGH_THRESHOLD:
-                            return best_value, best_move
-
-                        # Update alpha and prune if necessary
-                        alpha = max(alpha, best_value)
-                        if beta <= alpha:
-                            break  # pruning
-
-    # Logic for MIN player (i.e., the opponent)
-    else:
+    moves = order_moves(player,board)
+    if player == -1:
         best_value = float('inf')
-        
-        # Check all possible moves
-        for z in range(4):
-            for x in range(4):
-                for y in range(4):
-                    if board[z][x][y] == 0:  # if cell is empty
-                        board[z][x][y] = player  # make a move
-                        value, _ = alpha_beta(1, depth - 1, alpha, beta, board)  # simulate our turn
-                        board[z][x][y] = 0  # undo the move
-
-                        # Update best value and move if needed
-                        if value < best_value:
-                            best_value = value
-                            best_move = (z, x, y)
-
-                        # If a move is found that's "bad enough", break out
-                        print("CHECK MIN " + str(best_value <= -GOOD_ENOUGH_THRESHOLD))
-                        if best_value <= -GOOD_ENOUGH_THRESHOLD:
-                            return best_value, best_move
-
-                        # Update beta and prune if necessary
-                        beta = min(beta, best_value)
-                        if beta <= alpha:
-                            break  # pruning
-    
+        best_move = None
+        for move in moves:
+            z, x, y = move
+            board[z][x][y] = player
+            value, _ = alpha_beta(1, depth - 1, alpha, beta, board)
+            board[z][x][y] = 0
+            if value < best_value:
+                best_value = value
+                best_move = (z, x, y)
+            beta = min(beta, best_value)
+            if beta <= alpha:
+                break
+        return best_value, best_move
+    else:
+        best_value = float('-inf')
+        best_move = None
+        for move in moves:
+            z, x, y = move
+            board[z][x][y] = player
+            value, _ = alpha_beta(-1, depth - 1, alpha, beta,board)
+            board[z][x][y] = 0
+            if value > best_value:
+                best_value = value
+                best_move = (z, x, y)
+            alpha = max(alpha, best_value)
+            if beta <= alpha:
+                break
     return best_value, best_move
+
+# def alpha_beta(player, depth, alpha, beta, board):
+#     """
+#     Implement the alpha-beta pruning algorithm to find the best move.
+
+#     Args:
+#         player (int): 1 for the MAX player, -1 for the MIN player.
+#         depth (int): Depth to search for the alpha-beta pruning.
+#         alpha (float): Alpha value for the algorithm.
+#         beta (float): Beta value for the algorithm.
+#         board (list): The current state of the game board.
+
+#     Returns:
+#         tuple: Best value calculated by the algorithm and the best move as (z, x, y).
+#     """
+#     if depth == 0 or game_logic.check_win(1, board) or game_logic.check_win(-1, board):  # Terminal state or depth limit reached
+#         return game_logic.evaluate(board, depth), None
+
+#     best_move = None
+
+#     # Logic for MAX player (i.e., our player)
+#     if player == 1:
+#         best_value = float('-inf')
+        
+#         # Check all possible moves
+#         for z in range(4):
+#             for x in range(4):
+#                 for y in range(4):
+#                     if board[z][x][y] == 0:  # if cell is empty
+#                         board[z][x][y] = player  # make a move
+#                         value, _ = alpha_beta(-1, depth - 1, alpha, beta, board)  # simulate opponent's turn
+#                         board[z][x][y] = 0  # undo the move
+
+#                         # Update best value and move if needed
+#                         if value > best_value:
+#                             best_value = value
+#                             best_move = (z, x, y)
+
+#                         # If a move is found that's "good enough", break out
+#                         print("CHECK MAX " + str(best_value >= GOOD_ENOUGH_THRESHOLD))
+#                         if best_value >= GOOD_ENOUGH_THRESHOLD:
+#                             return best_value, best_move
+
+#                         # Update alpha and prune if necessary
+#                         alpha = max(alpha, best_value)
+#                         if beta <= alpha:
+#                             break  # pruning
+
+#     # Logic for MIN player (i.e., the opponent)
+#     else:
+#         best_value = float('inf')
+        
+#         # Check all possible moves
+#         for z in range(4):
+#             for x in range(4):
+#                 for y in range(4):
+#                     if board[z][x][y] == 0:  # if cell is empty
+#                         board[z][x][y] = player  # make a move
+#                         value, _ = alpha_beta(1, depth - 1, alpha, beta, board)  # simulate our turn
+#                         board[z][x][y] = 0  # undo the move
+
+#                         # Update best value and move if needed
+#                         if value < best_value:
+#                             best_value = value
+#                             best_move = (z, x, y)
+
+#                         # If a move is found that's "bad enough", break out
+#                         print("CHECK MIN " + str(best_value <= -GOOD_ENOUGH_THRESHOLD))
+#                         if best_value <= -GOOD_ENOUGH_THRESHOLD:
+#                             return best_value, best_move
+
+#                         # Update beta and prune if necessary
+#                         beta = min(beta, best_value)
+#                         if beta <= alpha:
+#                             break  # pruning
+    
+#     return best_value, best_move
+
+def order_moves(player,board):
+    moves = []
+    for z in range(4):
+        for x in range(4):
+            for y in range(4):
+                if board[z][x][y] == 0:
+                    score = 0  # Default score
+                    # Prioritize center cells
+                    if z in [1, 2] and x in [1, 2] and y in [1, 2]:
+                        score = 1 if player == 1 else -1
+                    moves.append(((z, x, y), score))
+
+    return [move for move, _ in sorted(moves, key=lambda item: item[1], reverse=(player == 1))]
--- game_logic.py
+++ game_logic.py
@@ -21,63 +21,23 @@ def check_win(player, board):
 
     # 2D Check:
     for z in range(4):
-        # Check rows and columns for each layer (z)
+        # Rows and columns
         for i in range(4):
-            if all(board[z][i][j] == player for j in range(4)) or \
-               all(board[z][j][i] == player for j in range(4)):
+            if all(board[z][i][j] == player for j in range(4)) or all(board[z][j][i] == player for j in range(4)):
                 return True
-
-        # Check two 2D diagonals for each layer (z)
-        if all(board[z][i][i] == player for i in range(4)) or \
-           all(board[z][i][3 - i] == player for i in range(4)):
+        # Diagonals
+        if all(board[z][i][i] == player for i in range(4)) or all(board[z][i][3 - i] == player for i in range(4)):
             return True
-
-    # 3D Check:
-    # Check vertical stack across the layers
+    # Check vertical columns
     for x in range(4):
         for y in range(4):
-            if all(board[k][x][y] == player for k in range(4)):
+            if all(board[z][x][y] == player for z in range(4)):
                 return True
-
-    # Check 3D main diagonals
-    # Top-left of top layer to bottom-right of bottom layer
-    if all(board[i][i][i] == player for i in range(4)):
+    # Check 3D diagonals
+    if all(board[i][i][i] == player for i in range(4)) or all(board[i][i][3 - i] == player for i in range(4)):
         return True
-
-    # Top-right of top layer to bottom-left of bottom layer
-    if all(board[i][3-i][i] == player for i in range(4)):
+    if all(board[i][3 - i][i] == player for i in range(4)) or all(board[i][3 - i][3 - i] == player for i in range(4)):
         return True
-
-    # Bottom-left of top layer to top-right of bottom layer
-    if all(board[i][i][3-i] == player for i in range(4)):
-        return True
-
-    # Bottom-right of top layer to top-left of bottom layer
-    if all(board[i][3-i][3-i] == player for i in range(4)):
-        return True
-
-    # Check 3D diagonals across rows and columns
-    # Diagonal top to bottom layer through each column
-    for y in range(4):
-        if all(board[i][3-i][y] == player for i in range(4)):
-            return True
-        
-    # Diagonal bottom to top layer through each column
-    for y in range(4):
-        if all(board[i][i][y] == player for i in range(4)):
-            return True
-
-    # Diagonal top to bottom layer through each row
-    for x in range(4):
-        if all(board[i][x][3-i] == player for i in range(4)):
-            return True
-
-    # Diagonal bottom to top layer through each row
-    for x in range(4):
-        if all(board[i][x][i] == player for i in range(4)):
-            return True
-
-    # If none of the above conditions were met, the player hasn't won
     return False
 
 
@@ -99,7 +59,7 @@ def is_board_full(board):
     return True
 
 
-def evaluate(board, depth):
+# def evaluate(board, depth):
     """
     Evaluate the game state.
     
@@ -113,9 +73,9 @@ def evaluate(board, depth):
     """
 
     # Check for immediate win/loss FIRST
-    if check_win(1, board):  # Player wins
+    if check_win(1,board):  # Player wins
         return -10000
-    elif check_win(-1, board):  # Opponent (AI) wins
+    elif check_win(-1,board):  # Opponent (AI) wins
         return -10000
 
     score = 0
@@ -191,3 +151,78 @@ def evaluate(board, depth):
 
     print("final " + str(score))
     return score
+
+"""
+    evaluate()
+    Evaluates the current state of the board and returns a score.
+    
+    Returns:
+    - int: The evaluation score of the board. 
+           A positive score is good for "X", a negative score is good for "O", and 0 indicates a neutral board.
+"""
+
+def evaluate(board):
+    if check_win(1,board):
+        return 100
+    elif check_win(-1,board):
+        return -100
+    else:
+        return evaluate_lines(board)
+
+"""
+    evaluate_lines()
+    Evaluates all lines (rows, columns, diagonals) on the board for their potential to lead to a win.
+    
+    Returns:
+    - int: The evaluation score based on the board lines.
+"""
+
+def evaluate_lines(board):
+    score = 0
+    for z in range(4):
+        for x in range(4):
+            for y in range(4):
+                if board[z][x][y] == 0:
+                    score += evaluate_direction(z, x, y, 1, 0, 0,board)  # Forward
+                    score += evaluate_direction(z, x, y, 0, 1, 0,board)  # Right
+                    score += evaluate_direction(z, x, y, 0, 0, 1,board)  # Up
+                    # Add other directions like diagonals, etc.
+    return score
+
+
+"""
+    evaluate_direction(z, x, y, dz, dx, dy)
+    Evaluates a specific direction from a given starting point on the board.
+    
+    Args:
+    - z, x, y (int): The starting point's layer, row, and column respectively.
+    - dz, dx, dy (int): The direction to move in the z, x, and y dimensions respectively.
+    
+    Returns:
+    - int: The evaluation score based on the board's direction.
+"""
+
+def evaluate_direction(z, x, y, dz, dx, dy,board):
+    player_count = 0
+    opponent_count = 0
+    for i in range(4):
+        if 0 <= z + dz*i < 4 and 0 <= x + dx*i < 4 and 0 <= y + dy*i < 4:
+            if board[z + dz*i][x + dx*i][y + dy*i] == 1:
+                player_count += 1
+            elif board[z + dz*i][x + dx*i][y + dy*i] == -1:
+                opponent_count += 1
+
+    if player_count == 3 and opponent_count == 0:
+        return 10
+    elif player_count == 2 and opponent_count == 0:
+        return 3
+    elif player_count == 1 and opponent_count == 0:
+        return 1
+    elif opponent_count == 3 and player_count == 0:
+        return -10
+    elif opponent_count == 2 and player_count == 0:
+        return -3
+    elif opponent_count == 1 and player_count == 0:
+        return -1
+    else:
+        return 0
